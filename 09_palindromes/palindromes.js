d0957aa649444545bc4f9eae7a250e1f90b560e7
bernardouribe98 <127761873+bernardouribe98@users.noreply.github.com>
2023-04-27 13:57:45 -0500

    Complete exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const regex = /[\W_]/g; // matches non-word characters and underscores
+    const cleanedStr = str.toLowerCase().replace(regex, ''); // converts to lowercase and removes non-word characters
+    const reversedStr = cleanedStr.split("").reverse().join("");
+    return cleanedStr === reversedStr;
 };
 
 // Do not edit below this line
