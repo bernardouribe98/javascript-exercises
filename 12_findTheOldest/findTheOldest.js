79eeba9d76f36ebb7a6834ce1a978bf3e3df49d6
bernardouribe98 <127761873+bernardouribe98@users.noreply.github.com>
2023-04-27 14:50:28 -0500

    Complete exercise 12_FINDTHEOLDEST

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
+    let oldestAge = -Infinity;
+    let oldestPerson = null;
+  
+    for (let person of people) {
+      const birthYear = person.yearOfBirth;
+      const deathYear = person.yearOfDeath || currentYear;
+      const age = deathYear - birthYear;
+  
+      if (age > oldestAge) {
+        oldestAge = age;
+        oldestPerson = person;
+      }
+    }
+  
+    return oldestPerson;
 };
 
 // Do not edit below this line
